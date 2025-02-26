abcce2f076f8edaefe2cb36fcfc3caa9000a807f
Volkin <volkin.jsa@gmail.com>
2025-02-26 12:45:32 +0100

    Firebase-config

--- src/main/java/com/lactancia/api/config/FirebaseConfig.java
+++ src/main/java/com/lactancia/api/config/FirebaseConfig.java
@@ -7,25 +7,39 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import java.io.ByteArrayInputStream;
+import java.io.FileInputStream;
 import java.io.IOException;
 
 @Configuration
 public class FirebaseConfig {
 
     @Bean
-    public void initializeFirebase() throws IOException {
-        String firebaseConfig = System.getenv("FIREBASE_CONFIG"); // Leer desde la variable de entorno
+    public FirebaseApp initializeFirebase() throws IOException {
+        // Obtener el valor de la variable de entorno FIREBASE_CONFIG
+        String firebaseConfig = System.getenv("FIREBASE_CONFIG");
 
         if (firebaseConfig == null || firebaseConfig.isEmpty()) {
-            throw new IOException("FIREBASE_CONFIG no está configurado");
+            throw new IOException("La variable de entorno FIREBASE_CONFIG no está configurada.");
         }
 
-        FirebaseOptions options = FirebaseOptions.builder()
-            .setCredentials(GoogleCredentials.fromStream(new ByteArrayInputStream(firebaseConfig.getBytes())))
-            .build();
-
-        if (FirebaseApp.getApps().isEmpty()) {
-            FirebaseApp.initializeApp(options);
+        FirebaseOptions options;
+        
+        if (firebaseConfig.startsWith("{")) {
+            // Si el valor de FIREBASE_CONFIG parece ser un JSON, lo tratamos como contenido directo
+            ByteArrayInputStream serviceAccount = new ByteArrayInputStream(firebaseConfig.getBytes());
+            
+            options = FirebaseOptions.builder()
+                    .setCredentials(GoogleCredentials.fromStream(serviceAccount))
+                    .build();
+            
+        } else {
+            // Si no, tratamos el valor como una ruta a un archivo
+            FileInputStream serviceAccount = new FileInputStream(firebaseConfig);
+            options = FirebaseOptions.builder()
+                    .setCredentials(GoogleCredentials.fromStream(serviceAccount))
+                    .build();
         }
+
+        return FirebaseApp.initializeApp(options);
     }
 }
